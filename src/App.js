d5b81d213b73aa073d7f4261a6f603d5db049093
Emmanuel Danso Annan <emmanueldansoannan@gmail.com>
2024-11-04 19:30:20 +0000

    done with home page for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { PageComponents } from "./routes";
 import Header from "./layouts/Header/Header";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from "./layouts/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route key={page.id} path={page.path} element={page.element} />
         ))}
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
