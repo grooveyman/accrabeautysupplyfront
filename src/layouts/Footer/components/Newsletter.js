e622ae117a7854110c30873fee530284b7ee51a3
Edannan <emmanueldansoannan@gmail.com>
2024-11-17 06:43:33 +0000

    footer and noresults component updates

--- src/layouts/Footer/components/Newsletter.js
+++ src/layouts/Footer/components/Newsletter.js
@@ -22,7 +22,7 @@ const Newsletter = () => {
               id="email"
               autoComplete="email"
               placeholder="Enter your email"
-              className="block w-full sm:max-w-md rounded-md border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              className="block w-full sm:max-w-md rounded-md border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6"
             />
           </div>
           <div>
