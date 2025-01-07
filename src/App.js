25568f3848605449935e8f924b3419f532e4cf35
Emmanuel Danso Annan <emmanueldansoannan@gmail.com>
2025-01-07 15:56:44 +0000

    fix: basename removed

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
   return (
     <AuthContextProvider>
       <ModalToggleProvider>
-        <BrowserRouter basename="/accrabeautysupplyfront">
+        <BrowserRouter>
           <AppWrapper />
         </BrowserRouter>
       </ModalToggleProvider>
